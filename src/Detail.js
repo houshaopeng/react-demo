dc943f0ad85ede3db51086680967ac498c891ff9
Hou <474064897@qq.com>
2018-04-20 16:40:16 +0800

    frist

---
+++ src/Detail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+class Detail extends Component {
+    render() {
+      return (
+        <div className="detail">
+          
+          <p className="">
+                frist page
+          </p>
+        </div>
+      );
+    }
+  }
+  
