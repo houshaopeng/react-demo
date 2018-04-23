bdabd761f3caff250e0d9f020c03d4c0fe95fc4f
Hou <474064897@qq.com>
2018-04-23 15:43:03 +0800

    11

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class Record extends Component {
+  constructor(){
+    super();
+    this.state = {
+      
+    }
+  }
+
   render() {
     return (
       <div>
