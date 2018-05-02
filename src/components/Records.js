a90e2cf1ff4ff523d328a6090e64d620f57651aa
Hou <474064897@qq.com>
2018-05-02 15:39:21 +0800

    11

--- src/components/Records.js
+++ src/components/Records.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import Record from './Record';
 class Records extends Component {
+  constuctor(){
+    super();
+    this.state = {
+      record:'sadasd',
+    }
+  }
   render() {
     return (
       <div>
         <h2>Records</h2>
         <div className="table"></div>
-        {/* this.state ={
-          constuctor(){
-            super();
-            this.state = {
-              record:'sadasd',
-            }
-          }
-        } */}
+        
         <Record/>  
       </div>
     );
