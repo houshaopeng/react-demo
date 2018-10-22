75a6c7008ff70881428ec1fe4a509b5c71df5e48
Hou <474064897@qq.com>
2018-10-22 13:49:48 +0800

    22

--- src/Detail.js
+++ src/Detail.js
@@ -6,7 +6,7 @@ class Detail extends Component {
         <div className="detail">
           
           <p className="">
-                frist page 10.19
+                frist page 10.22
           </p>
         </div>
       );
